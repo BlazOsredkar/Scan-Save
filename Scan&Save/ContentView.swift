47bb2f3d0e13870ddcca18cc1af03a3a4d97b21d
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2023-10-04 10:40:28 +0200

    Initial Commit

---
+++ Scan&Save/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scan&Save
+//
+//  Created by Blaž Osredkar on 4. 10. 23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scan&Save/Scan_SaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scan_SaveApp.swift
+//  Scan&Save
+//
+//  Created by Blaž Osredkar on 4. 10. 23.
+//
+
+import SwiftUI
+
+@main
+struct Scan_SaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
